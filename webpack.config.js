93ca2ee78eb9d90e4c7e79a33ddaf2f2c8c2d17f
Viktor <v_kozintcev@mail.ru>
2024-11-03 18:33:09 +0300

    adding rules and setting up tests

--- webpack.config.js
+++ webpack.config.js
@@ -23,9 +23,6 @@ const config = {
     }),
 
     new MiniCssExtractPlugin(),
-
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
   module: {
     rules: [
@@ -42,9 +39,6 @@ const config = {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: "asset",
       },
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
   },
   resolve: {
